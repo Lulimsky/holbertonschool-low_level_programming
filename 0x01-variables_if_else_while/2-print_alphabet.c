a19c24a390cf35a136991104aafb2982a07641ca
lulimsky <3337@holbertonschool.com>
2021-10-03 13:11:33 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - put alphabet in lowecase.
+ *
+ * Return : Always 0.
+ */
+int main() {
+    char c;
+    for (c = 'a'; c <= 'z'; ++c){
+        putchar(c);
+    }
+        putchar('\n');
+ 	return(0);       
+}
+

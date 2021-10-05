d05a03b53165fb174e50d09572f07a8ce7178351
lulimsky <3337@holbertonschool.com>
2021-10-05 13:40:50 -0700

    2-print_alphabet_x10.cII

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - abc ten times. 
+ * print_alphabet_x10 - count the abc ten times.
  *
  * Return: to void.
  */
@@ -10,10 +10,10 @@ void print_alphabet_x10(void)
 	int cu;
 	int al;
 
-	for(cu = 0 ; cu >= 10 ; cu++)
-	{	
-		for(al = 'a' ; al >= 'z'; al++)
+	for (cu = 0 ; cu <= 10 ; cu++)
+	{
+		for (al = 'a' ; al <= 'z'; al++)
 			_putchar (al);
 		_putchar ('\n');
-	}	
+	}
 }

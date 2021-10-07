c4c812d7b192bc6d4c4683e2cf90ba931028f9a1
lulimsky <3337@holbertonschool.com>
2021-10-07 10:30:08 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * more_numbers - count the abc ten times.
+ *@
+ *@
+ * Return: to void.
+ */
+void more_numbers(void)
+{
+	int c;
+	int n;
+
+	for (c = 0 ; c <= 10 ; c++)
+	{
+		for (n = 0 ; n <= 14; n++)
+			_putchar (n);
+		_putchar ('\n');
+	}
+}

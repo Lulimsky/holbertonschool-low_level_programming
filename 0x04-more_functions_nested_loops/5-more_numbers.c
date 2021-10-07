f0932f10eb025f7446443d4149db3fd8d4633588
lulimsky <3337@holbertonschool.com>
2021-10-07 11:51:03 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,8 @@
-#include"main.h"
-
+#include "main.h"
 /**
- * more_numbers - count the abc ten times.
- *@
- *@
+ * more_numbers - print a counter fourteen times.
+ *@int c.
+ *@int n.
  * Return: to void.
  */
 void more_numbers(void)
@@ -13,8 +12,15 @@ void more_numbers(void)
 
 	for (c = 0 ; c <= 10 ; c++)
 	{
-		for (n = 0 ; n <= 14; n++)
-			_putchar (n);
-		_putchar ('\n');
+		for (n = 0 ; n < 15 ; n++)
+		{
+			if (c >= 10)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
+		}
+		_putchar(10);
 	}
+
 }

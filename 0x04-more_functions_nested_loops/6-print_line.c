33be0c568cf29c9e3de06e08450bf212f02402d0
lulimsky <3337@holbertonschool.com>
2021-10-08 12:50:23 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,24 @@
-wdncoajednmlKI
+#include "main.h"
+
+/**
+ *print_line - draws a straight line.
+ *@n: int.
+ * Return: to void.
+ */
+void print_line(int n)
+{
+	int c;
+
+	if (n <= 0)
+	{
+		_putchar (10);
+	}
+	else
+
+	for (c = 0 ; c < n ; c++)
+	{
+		_putchar('_');
+	}
+		_putchar(10);
+
+}

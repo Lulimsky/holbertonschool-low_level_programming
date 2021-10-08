7ed476a3f1340c2d8065e666c6f6303fa2986941
lulimsky <3337@holbertonschool.com>
2021-10-08 11:41:16 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_numbers - print a count to zero to nine..
+*
+* Return: to void.
+*/
+void print_most_numbers(void)
+{
+	int count;
+
+	for (count = 0 ; count <= 9; count++)
+	{	if (count != 2 && 4)
+		
+		{ 
+			_putchar (count + '0');
+		}
+	}
+	_putchar ('\n');
+
+}

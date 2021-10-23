2f3901464d795c1307d5ea7ccebcf51fd445f32f
lulimsky <3337@holbertonschool.com>
2021-10-22 18:39:57 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - function that multiplies two numbers.
+* @argc: argument count.
+* @argv: argument vector.
+* Return: one or zero.
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
+		else
+	{
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 
 int _putchar(char c);
-
+int main (int argc, char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc __attribute__((unused)), char *envp[]);
+int _atoi(char *s);
 
 #endif /* MAIN_H */

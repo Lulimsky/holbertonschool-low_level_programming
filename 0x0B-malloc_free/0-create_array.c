edabb5381eaf97a494b3f422f653ff2e1bccf2de
lulimsky <3337@holbertonschool.com>
2021-10-26 14:46:16 -0700

    task-0.1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * create_array -creates an array of chars, starting with an specific char.
+  * @size: The size of the array
+  * @c: The char to fill in the array
+  *
+  * Return: pointer to the array, or NULL if it fails.
+  */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int index;
+	char *ptr;
+
+	if (size == 0)
+		return (NULL);
+
+	ptr = malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; ptr++)
+	{
+		ptr[index] = c;
+	}
+
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - contains a copy of the string given as a parameter.
+ * @str: string
+ * Return: pointer to newly allocated space in memory.
+ */
+char *_strdup(char *str);
+unsigned int index, indice;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	for (index = 0; str[index]; index++)
+		;
+	index++;
+	ptr = malloc(index * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (indice = 0; indice < index; indice++)
+	{
+		ptr[indice] = str[indice];
+	}
+	return (ptr);

0a123856730422429f0b25a440f13051d18ebb53
lulimsky <3337@holbertonschool.com>
2021-11-03 14:46:01 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int count;
 
-	if (size <= 0 && cmp != NULL && array != NULL)
+	if (size > 0 && cmp != NULL && array != NULL)
 	{
 		return (-1);
 	}

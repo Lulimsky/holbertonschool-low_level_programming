ec9e4871c2ab46ea60a914a8906cd95858cdd7a0
lulimsky <3337@holbertonschool.com>
2021-12-13 10:52:36 -0800

    64

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <limits.h> 
+#include <limits.h>
 /**
  * set_bit - function that sets the value of a bit at a given index
  * @n: pointer to number to be chaged
@@ -9,10 +9,8 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-if (*n > ULONG_MAX)
-{
+if ((*n > ULONG_MAX) || (index >= 64))
 return (-1);
-}
 
 *n |= 1 << index;
 return (1);

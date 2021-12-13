178ddb53b1ccaea55fdee6c97c0de8e69f1ce040
lulimsky <3337@holbertonschool.com>
2021-12-13 09:32:42 -0800

    checker

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,33 @@
 #include "main.h"
 
+/**
+ * print_binary - function that prints an int in bin
+ * @n: int
+ */
+
 void print_binary(unsigned long int n)
 {
-  int c, k;
-  int first_time = 0;
-  
-  for (c = 31; c >= 0; c--)
-  {
-    k = n >> c;
+int c, k;
+int first_time = 0;
+if (n == 0)
+{
+_putchar('0');
+return;
+}
 
-    if (k & 1)
-    {
-      _putchar('1');
-      first_time = 1;
-    }
-    else
-    {
-      if (first_time)
-        _putchar('0');
-    }
-  }
+for (c = 31; c >= 0; c--)
+{
+k = n >> c;
 
-  _putchar('\n');
-
-}	
+if (k & 1)
+{
+_putchar('1');
+first_time = 1;
+}
+else
+{
+if (first_time)
+_putchar('0');
+}
+}
+}

a327e2857f0ecdbabb581983819314a37153f125
lulimsky <3337@holbertonschool.com>
2021-12-13 09:00:19 -0800

    define

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,4 +1,9 @@
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+
+#endif

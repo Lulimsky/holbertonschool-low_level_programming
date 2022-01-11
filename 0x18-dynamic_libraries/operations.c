9d4b3fc63859982e759de3a9a8e5b2b64677e312
lulimsky <3337@holbertonschool.com>
2022-01-11 11:32:52 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = (a * b);
+
+	return (mul);
+}
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+int sub(int a, int b)
+{
+	return (a - b);
+
+}
+int mod(int a, int b)
+{
+	return (a % b);
+}
